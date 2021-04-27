cbfb8a18ed107d43896f95c41cb9e831782a1aeb
Alexey Zhelezov <zhelezovalexey@gmail.com>
2021-04-27 17:22:20 +0200

    added signature pad

---
+++ layouts/empty.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app light>
+    <nuxt />
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+  @Component
+  export default class Empty extends Vue {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/signature/_id.vue
@@ -0,0 +1,24 @@
+<template>
+  <client-only>
+    <vueSignature ref="signature" :w="'800px'" :h="'400px'" />
+  </client-only>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import vueSignature from 'vue-signature';
+
+  @Component({
+    layout: 'empty',
+    components: {
+      vueSignature
+    }
+  })
+  export default class SignaturePad extends Vue {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
